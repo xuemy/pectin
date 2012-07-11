a729001aba815c21ff14c176649578ae8820d571
tioover <tioover@gmail.com>
2012-07-11 19:43:31 +0800

    forms example

--- website/application.py
+++ website/application.py
@@ -1,9 +1,15 @@
 from tornado import web
 from pectin.web import MediaApplicationMixin, TemplateApplicationMixin
 #from pectin.database import SQLAlchemy
+from pectin import forms
 from tornado import httpserver, ioloop, options
+from wtforms import TextField
 
 #database = SQLAlchemy("sqlite:///test.sqlite")
+
+
+class TestForm(forms.Form):
+    text = TextField("Text Field")
 
 
 class HelloHandler(web.RequestHandler):
@@ -11,10 +17,20 @@ class HelloHandler(web.RequestHandler):
         self.render("home.html")
 
 
+class FormsTestHandler(web.RequestHandler):
+    Form = TestForm
+
+    def get(self):
+        self.render("forms.html")
+
+
 class Application(TemplateApplicationMixin, MediaApplicationMixin,
         web.Application):
     def __init__(self):
-        handlers = [("/", HelloHandler)]
+        handlers = [
+            ("/", HelloHandler),
+            ("/forms/?", FormsTestHandler),
+        ]
         super(Application, self).__init__(handlers, debug=True,
                 template_path="templates", media_path="media")
 
